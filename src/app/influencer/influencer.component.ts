6942f5b01fb4d5eb10f7c86fbd6dc58c904ac465
pranavgnr <gnrpranav@gmail.com>
2024-10-15 20:32:41 +0530

    for mobile devices

--- src/app/influencer/influencer.component.ts
+++ src/app/influencer/influencer.component.ts
@@ -52,8 +52,8 @@ export class InfluencerComponent {
       end: 'bottom center-100', // End when the bottom of the container hits the center of the viewport
       scrub: true, // Smoothly links the animation to the scroll position
     },
-    height: '95%', // The final height of the line
-    duration: 0.2,
+    height: '90%', // The final height of the line
+    duration: 2,
     ease: 'none' // Keep the speed constant
   });
 
